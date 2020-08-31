fd85f67fad4ac994cf9e2be846529c9587b097d7
Krnik <marcin11qwerty@gmail.com>
2020-08-31 22:37:13 +0200

    init: nvim

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,17 @@
 fish_user_key_bindings
 
-set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_CACHE_HOME $HOME/.local/cache
-set -gx XDG_RUNTIME_HOME $HOME/.local/runtime
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_CACHE_HOME $HOME/.local/cache
+set -x XDG_RUNTIME_HOME $HOME/.local/runtime
 
-set -gx CARGO_HOME $XDG_CONFIG_HOME/cargo
-set -gx RUSTUP_HOME $XDG_CONFIG_HOME/rustup
-set -gx BAT_CONFIG_PATH $XDG_CONFIG_HOME/bat/bat.conf
+set -x CARGO_HOME $XDG_CONFIG_HOME/cargo
+set -x RUSTUP_HOME $XDG_CONFIG_HOME/rustup
+set -x BAT_THEME gruvbox
 
-set -gx NVM_DIR $XDG_CONFIG_HOME/nvm
-set -gx NPM_CONFIG_PREFIX $XDG_CONFIG_HOME/npm
-set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/.npmrc
-set -gx NPM_CONFIG_CACHE $XDG_CONFIG_HOME/npm
+set -x NVM_DIR $XDG_CONFIG_HOME/nvm
+set -x NPM_CONFIG_PREFIX $XDG_CONFIG_HOME/npm
+set -x NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/.npmrc
+set -x NPM_CONFIG_CACHE $XDG_CONFIG_HOME/npm
 
-set -gx PATH $CARGO_HOME/bin:$HOME/apps/nvim/bin:$NPM_CONFIG_PREFIX/bin $PATH
+set -x PATH $CARGO_HOME/bin:$HOME/apps/nvim/bin:$NPM_CONFIG_PREFIX/bin $PATH
--- fish/functions/toggle_theme.fish
+++ fish/functions/toggle_theme.fish
@@ -4,35 +4,8 @@ function toggle_theme
     end
 
     if test $fish_color_theme_is_dark = 1
-        set -U fish_color_normal normal
-        set -U fish_color_command 99cc99
-        set -U fish_color_quote ffcc66
-        set -U fish_color_redirection d3d0c8
-        set -U fish_color_end cc99cc
-        set -U fish_color_error f2777a
-        set -U fish_color_param d3d0c8
-        set -U fish_color_comment ffcc66
-        set -U fish_color_match 6699cc
-        set -U fish_color_selection white --bold --background=brblack
-        set -U fish_color_search_match bryellow --background=brblack
-        set -U fish_color_history_current --bold
-        set -U fish_color_operator 6699cc
-        set -U fish_color_escape 66cccc
-        set -U fish_color_cwd green
-        set -U fish_color_cwd_root red
-        set -U fish_color_valid_path --underline
-        set -U fish_color_autosuggestion 747369
-        set -U fish_color_user brgreen
-        set -U fish_color_host normal
-        set -U fish_color_cancel -r
-        set -U fish_pager_color_completion normal
-        set -U fish_pager_color_description B3A06D yellow
-        set -U fish_pager_color_prefix white --bold --underline
-        set -U fish_pager_color_progress brwhite --background=cyan
+        theme_gruvbox light medium
 
-        set -g fish_color_theme_is_dark 0
-        theme_gruvbox dark medium
-    else
         set -U fish_color_normal normal
         set -U fish_color_command a1b56c
         set -U fish_color_quote f7ca88
@@ -59,7 +32,38 @@ function toggle_theme
         set -U fish_pager_color_prefix white --bold --underline
         set -U fish_pager_color_progress brwhite --background=cyan
 
+        set -x BAT_THEME gruvbox-light
+        set -g fish_color_theme_is_dark 0
+    else
+        theme_gruvbox dark medium
+
+        set -U fish_color_normal normal
+        set -U fish_color_command 99cc99
+        set -U fish_color_quote ffcc66
+        set -U fish_color_redirection d3d0c8
+        set -U fish_color_end cc99cc
+        set -U fish_color_error f2777a
+        set -U fish_color_param d3d0c8
+        set -U fish_color_comment ffcc66
+        set -U fish_color_match 6699cc
+        set -U fish_color_selection white --bold --background=brblack
+        set -U fish_color_search_match bryellow --background=brblack
+        set -U fish_color_history_current --bold
+        set -U fish_color_operator 6699cc
+        set -U fish_color_escape 66cccc
+        set -U fish_color_cwd green
+        set -U fish_color_cwd_root red
+        set -U fish_color_valid_path --underline
+        set -U fish_color_autosuggestion 747369
+        set -U fish_color_user brgreen
+        set -U fish_color_host normal
+        set -U fish_color_cancel -r
+        set -U fish_pager_color_completion normal
+        set -U fish_pager_color_description B3A06D yellow
+        set -U fish_pager_color_prefix white --bold --underline
+        set -U fish_pager_color_progress brwhite --background=cyan
+
+        set -x BAT_THEME gruvbox
         set -g fish_color_theme_is_dark 1
-        theme_gruvbox light medium
     end
 end
