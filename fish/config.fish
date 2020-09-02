392d614d5f1851b02b62e20e6385587b63a8e7c9
Krnik <marcin11qwerty@gmail.com>
2020-09-02 21:29:58 +0200

    init: fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,17 @@
-set -g fish_color_theme_is_dark 1
+if set -q BAT_THEME
+    switch $BAT_THEME
+        case 'gruvbox'
+            fish_theme set dark
+            set -g fish_color_theme_is_dark 1
+        case 'gruvbox-light'
+            fish_theme set light
+            set -g fish_color_theme_is_dark 0
+    end
+else
+    fish_theme set dark
+    set -g fish_color_theme_is_dark 1
+    set -x BAT_THEME gruvbox
+end
 
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_DATA_HOME $HOME/.local/share
@@ -7,7 +20,6 @@ set -x XDG_RUNTIME_HOME $HOME/.local/runtime
 
 set -x CARGO_HOME $XDG_CONFIG_HOME/cargo
 set -x RUSTUP_HOME $XDG_CONFIG_HOME/rustup
-set -x BAT_THEME gruvbox
 
 set -x NVM_DIR $XDG_CONFIG_HOME/nvm
 set -x NPM_CONFIG_PREFIX $XDG_CONFIG_HOME/npm
